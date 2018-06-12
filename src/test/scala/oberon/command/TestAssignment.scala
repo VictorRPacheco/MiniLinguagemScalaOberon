185b1dab6031e966a89dae08068752ec2d085b1b
paulovgf <pve1356@gmail.com>
2018-06-12 16:34:31 -0300

    Fixing assignment test

--- src/test/scala/oberon/command/TestAssignment.scala
+++ src/test/scala/oberon/command/TestAssignment.scala
@@ -7,6 +7,7 @@ import org.scalatest.BeforeAndAfter
 
 
 import oberon.Environment._
+import oberon.callable.Variable
 import oberon.expression.IntValue
 
 class TestAssignment extends FlatSpec with Matchers with GivenWhenThen with BeforeAndAfter {
@@ -17,11 +18,17 @@ class TestAssignment extends FlatSpec with Matchers with GivenWhenThen with Befo
     clear()
   }
 
-  it should "the environment must have an assignment x -> 5" in { 
-    val assignment = new Assignment("x", IntValue(5))
+  it should "the environment must have an assignment x -> 5" in {
+    val variableX = new VariableDefinition(new Variable("x", "Integer", IntValue(5)))
+    val assignment = new Assignment("x", IntValue(10))
 
+    variableX.run()
     assignment.run()
 
-    lookup("x") should be (Some(IntValue(5))) 
+    val res = lookup("x")
+    res match {
+      case Some(v) => v.asInstanceOf[Variable].dataValue.eval() should be (IntValue(10))
+      case _       => println("Erros")
+    }
   }
 }
